c4aba35e18ef719f2e46766f16edf7d57df06c7d
mei23 <m@m544.net>
2024-02-18 18:16:57 +0900

    Proxyキャッシュ7日

--- src/server/file/index.ts
+++ src/server/file/index.ts
@@ -21,14 +21,14 @@ router.get('/default-avatar.jpg', ctx => {
 	const file = fs.createReadStream(`${__dirname}/assets/avatar.jpg`);
 	ctx.body = file;
 	ctx.set('Content-Type', 'image/jpeg');
-	ctx.set('Cache-Control', 'max-age=2592000, s-maxage=172800, immutable');
+	ctx.set('Cache-Control', 'max-age=2592000, s-maxage=604800, immutable');
 });
 
 router.get('/app-default.jpg', ctx => {
 	const file = fs.createReadStream(`${__dirname}/assets/dummy.png`);
 	ctx.body = file;
 	ctx.set('Content-Type', 'image/jpeg');
-	ctx.set('Cache-Control', 'max-age=2592000, s-maxage=172800, immutable');
+	ctx.set('Cache-Control', 'max-age=2592000, s-maxage=604800, immutable');
 });
 
 router.get('/:id', sendDriveFile);
