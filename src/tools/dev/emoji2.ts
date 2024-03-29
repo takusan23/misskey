11a0219e26c779e97b176ff419666649438a1923
MeiMei <30769358+mei23@users.noreply.github.com>
2024-03-29 23:59:14 +0900

    Unicode Emoji 15.1 (#4845)

--- src/tools/dev/emoji2.ts
+++ src/tools/dev/emoji2.ts
@@ -1,27 +1,16 @@
 /* eslint-disable node/no-unpublished-require */
 // JSONのテンプレ作るスクリプト
 
-export const data = `face	1FAE8	shaking face
-symbols	1FA77	pink heart
-symbols	1FA75	light blue heart
-symbols	1FA76	grey heart
-people	1FAF7	leftwards pushing hand
-people	1FAF8	rightwards pushing hand
-animals_and_nature	1FACE	moose
-animals_and_nature	1FACF	donkey
-animals_and_nature	1FABD	wing
-animals_and_nature	1F426 200D 2B1B	black bird
-animals_and_nature	1FABF	goose
-animals_and_nature	1FABC	jellyfish
-animals_and_nature	1FABB	hyacinth
-food_and_drink	1FADA	ginger root
-food_and_drink	1FADB	pea pod
-objects	1FAAD	folding hand fan
-objects	1FAAE	hair pick
-activity	1FA87	maracas
-activity	1FA88	flute
-symbols	1FAAF	khanda
-symbols	1F6DC	wireless`;
+export const data = `face	1F642 200D 2194 FE0F	head shaking horizontally
+face	1F642 200D 2195 FE0F	head shaking vertically
+people	1F9D1 200D 1F9D1 200D 1F9D2	family adult adult child
+people	1F9D1 200D 1F9D1 200D 1F9D2 200D 1F9D2	family adult adult child child
+people	1F9D1 200D 1F9D2	family adult child
+people	1F9D1 200D 1F9D2 200D 1F9D2	family adult child child
+animals_and_nature	1F426 200D 1F525	phoenix
+food_and_drink	1F34B 200D 1F7E9	lime
+food_and_drink	1F344 200D 1F7EB	brown mushroom
+objects	26D3 FE0F 200D 1F4A5	broken chain`;
 
 for (const line of data.split(/\n/)) {
 	const m = line.match(/^([0-9A-Za-z_]+)\t([0-9A-Fa-f ]+)\t([0-9A-Za-z ]+)$/);
